f61d5fd15bec6e5049d0fdbc3e95dedaf0e546cb
David Middleton <middleton.davida@gmail.com>
2022-03-22 11:16:10 -0400

    created search.js, basic text added to test routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Home from './home'
+import Home from './home';
+import Search from './search';
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <header className="App-header">
       </header>
       <Home />
+      <Search />
     </div>
   );
 }
