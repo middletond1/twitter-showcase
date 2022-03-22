92dfa9d8f3a63dde4ef0aeccabd76f27cf3428b1
David Middleton <middleton.davida@gmail.com>
2022-03-22 12:18:29 -0400

    installed React Router. Implemented within app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './home';
 import Search from './search';
+import { Link, Outlet } from 'react-router-dom';
+
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-      <Home />
-      <Search />
+      <nav className="Nav">
+        <h1>Navbar</h1>
+        <Link to='/home'>Home</Link>
+        <Link to='/search'>Search</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { 
+  BrowserRouter,
+  Routes,
+  Route,
+ } from 'react-router-dom';
+import Home from './home';
+import Search from './search';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='home' element={<Home />} />
+        <Route path='search' element={<Search />} />
+        <Route path="*" element={<p>There's nothing here!</p>}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
