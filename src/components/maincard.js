7b5ff2ac06b89835a4d325afb2d632ce463d5ee8
David Middleton <middleton.davida@gmail.com>
2022-03-24 12:34:23 -0400

    footer in Container

--- src/components/maincard.js
+++ src/components/maincard.js
@@ -2,13 +2,14 @@ import React from "react";
 import heartIco from "../img/heart.png";
 import retweetIco from "../img/retweet.png";
 import './maincard.css';
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function MainCard(props) {
     
     return (
         <div className="card">
             <header>
-                <img src='../img/heart.png' className="prof-Img" />
+                <img src={heartIco} className="prof-img" />
                 <h3 className="prof-name"></h3>
                 <h3 className="prof-screen-name"></h3>
                 <h3 className="post-time"></h3>
@@ -16,11 +17,23 @@ export default function MainCard(props) {
             <main>
                 <p className="post-text"></p>
             </main>
-            <footer>
-                <img src={retweetIco} className="retweet-img" />
-                <p className="retweet-count">250</p>
-                <img src={heartIco} className="heart-img" />
-                <p className="favorite-count">400</p>
+            <footer className="card-footer">
+                <Container>
+                    <Row>
+                        <Col className="">
+                            <img src={retweetIco} className="retweet-img" />
+                        </Col>
+                        <Col>
+                            <p className="retweet-count mb-0">250</p>
+                        </Col>
+                        <Col>
+                            <img src={heartIco} className="heart-img" />
+                        </Col>
+                        <Col>
+                            <p className="favorite-count mb-0">400</p>
+                        </Col>
+                    </Row>
+                </Container>
             </footer>
         </div>
     )
