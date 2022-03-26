a68e267c38dd7a64e94b865850a0ac93cb370413
David Middleton <middleton.davida@gmail.com>
2022-03-26 14:30:41 -0400

    moved routes to App.js from index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React, { useState } from 'react';
 import Home from './components/home';
 import Search from './components/search';
+import Random from './components/random';
 import NavComponent from './components/navbar';
-import { Outlet } from 'react-router-dom';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Routes
+ } from 'react-router-dom';
 
 export default function App() {
+  
+  const [searchTerm, setSearchTerm] = useState([]);
+
   return (
-    <div className="app">
-      <NavComponent />
-      <Outlet />
-    </div>
+    <Router>
+      <div className="app">
+        <NavComponent />
+          <div className='content'>
+            <Routes>
+                <Route path='home' element={<Home />} />
+                <Route path='search' element={<Search />} />
+                <Route path='random' element={<Random />} />
+                <Route path="*" element={<p>There's nothing here!</p>}/>
+            </Routes>
+          </div>
+      </div>
+    </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,28 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { 
-  BrowserRouter,
-  Routes,
-  Route,
- } from 'react-router-dom';
-import Home from './components/home';
-import Search from './components/search';
-import Random from './components/random';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />}>
-        <Route path='home' element={<Home />} />
-        <Route path='search' element={<Search />} />
-        <Route path='random' element={<Random />} />
-        <Route path="*" element={<p>There's nothing here!</p>}/>
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <App />,
   document.getElementById('root')
 );
 
