021f654483e8a9077c48985f3faaa2a4dd91a203
David Middleton <middleton.davida@gmail.com>
2022-03-29 20:13:00 -0400

    removed unnecessary imports in searchpage.js

--- src/components/searchpage.js
+++ src/components/searchpage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TweetCard from "./tweetcard";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import SearchBar from "./searchbar";
 
